a0c13567491f975776ff71caba36060b169c1641
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2023-02-16 15:55:25 +0300

    Modified file

--- src/test/java/ru/netology/test/ChangeDateTest.java
+++ src/test/java/ru/netology/test/ChangeDateTest.java
@@ -3,11 +3,11 @@ package ru.netology.test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Test;
 import ru.netology.utils.DataGenerator;
 import java.time.Duration;
 import static com.codeborne.selenide.Condition.*;
@@ -16,6 +16,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class ChangeDateTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setUP() {
